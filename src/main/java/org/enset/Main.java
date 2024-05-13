a2ae7ed583fde235d57893007c737842e4a90500
mohamedbenyghil <med.2016.ben@gmail.com>
2024-05-13 17:41:14 +0100

    first commit

---
+++ src/main/java/org/enset/Main.java
@@ -0,0 +1,32 @@
+package org.enset;
+
+import org.enset.Model.Ex1.StorageGenerique;
+import java.util.ArrayList;
+import java.util.List;
+public class Main {
+    public static void main(String[] args) {
+        // Test with Integer
+        StorageGenerique<Integer> intStorage = new StorageGenerique<>();
+        intStorage.addElement(1);
+        intStorage.addElement(2);
+        intStorage.addElement(3);
+        System.out.println("Integer Storage Size: " + intStorage.getSize());
+        System.out.println("Element at index 1: " + intStorage.getElement(1));
+        intStorage.removeElement(1);
+        System.out.println("Integer Storage Size after removal: " + intStorage.getSize());
+
+        // Test with String
+        StorageGenerique<String> stringStorage = new StorageGenerique<>();
+        stringStorage.addElement("Hello");
+        stringStorage.addElement("World");
+        System.out.println("\nString Storage Size: " + stringStorage.getSize());
+        System.out.println("Element at index 0: " + stringStorage.getElement(0));
+
+        // Test with Double
+        StorageGenerique<Double> doubleStorage = new StorageGenerique<>();
+        doubleStorage.addElement(3.14);
+        doubleStorage.addElement(2.718);
+        System.out.println("\nDouble Storage Size: " + doubleStorage.getSize());
+        System.out.println("Element at index 1: " + doubleStorage.getElement(1));
+    }
+}
